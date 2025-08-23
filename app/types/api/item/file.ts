3d82e23c950ec0236dc6717071efdda47a738220
Benoît VIGNAL <git@benoit-vignal.fr>
2025-08-23 15:55:01 +0200

    Add missing timestampable file

--- app/types/api/item/file.ts
+++ app/types/api/item/file.ts
@@ -1,6 +1,7 @@
 import type {UuidItem} from "~/types/api/uuidItem";
+import type {TimestampItem} from "~/types/api/timestampItem";
 
-export interface File extends UuidItem {
+export interface File extends UuidItem, TimestampItem {
   publicUrl?: string
   publicInlineUrl?: string
   privateUrl?: string
