344d114a04194a16d6a6dab63166f8b89fdf6ce6
Benoît VIGNAL <git@benoit-vignal.fr>
2026-05-22 19:30:50 +0200

    Improve stock output

--- app/pages/admin/sales/new.vue
+++ app/pages/admin/sales/new.vue
@@ -256,9 +256,9 @@ definePageMeta({
                 <div v-if="item.description" class="text-xs print:hidden">{{ item.description }}</div>
               </div>
               <div
-                v-if="item.quantityAlert && (item.quantity || item.quantity === 0) && item.quantity <= item.quantityAlert"
-                class="print:hidden text-xs font-bold text-error-600">
-                Stock restant : {{ item.quantity }}
+                v-if="(item.quantity || item.quantity === 0)"
+                :class="item.quantityAlert && item.quantity <= item.quantityAlert ? 'print:hidden text-xs font-bold text-error-600' : 'print:hidden text-xs opacity-50 hover:opacity-100'">
+                Stock : {{ item.quantity }}
               </div>
               <div data-testid="item-price" class="text-xs bg-neutral-200 print:!bg-neutral-200 dark:bg-neutral-600 p-1 rounded-md">{{ formatMonetary(item.sellingPrice) }}</div>
               <UButton class="print:hidden" data-testid="add-to-cart" icon="i-heroicons-shopping-cart" size="xs" @click="cartStore.addToCart(item)" />
